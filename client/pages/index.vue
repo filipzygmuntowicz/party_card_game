db7af8f9c65be9dc935e09d553a5f7a285a5b9b9
Sebastian <sebeklis132@gmail.com>
2022-05-03 19:27:56 +0200

    +reset.css +scss

---
+++ client/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <MainHero />
+</template>
+
+<style lang="scss">
+body {
+	background: linear-gradient(-45deg, #ee7752, #e73c7e, #5E0C46, #0B163A);
+	background-size: 400% 400%;
+	animation: gradient 20s ease infinite;
+	height: 100vh;
+}
+
+@keyframes gradient {
+	0% {
+		background-position: 0% 50%;
+	}
+	50% {
+		background-position: 100% 50%;
+	}
+	100% {
+		background-position: 0% 50%;
+	}
+}
+
+</style>
