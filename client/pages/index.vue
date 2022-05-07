43d98552368b236a9edd5fe978e777b894aedc6b
Sebastian <sebeklis132@gmail.com>
2022-05-07 11:34:29 +0200

    + head metatags, + favicon

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -7,6 +7,16 @@
 	</div>
 </template>
 
+<script setup>
+useHead({
+  title: `Party card game`,
+  meta: [{
+    name: 'description',
+    content: `Drkining card game, play with your friends!`
+  }]
+})
+</script>
+
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;900&family=Raleway:wght@700;900&display=swap');
 body {
@@ -30,5 +40,4 @@ body {
 		background-position: 0% 50%;
 	}
 }
-
 </style>
